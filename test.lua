98bf3694b958c69c17e30d9bcfb6507bd9d979f9
HilalWare-Official <hilalware.lua@gmail.com>
2025-11-10 23:04:45 +0300

    Update script loading URL in test.lua

--- test.lua
+++ test.lua
@@ -4,7 +4,7 @@ local teleportFonk = (syn and syn.queue_on_teleport) or queue_on_teleport
 if teleportFonk then
     teleportFonk([[
         print("selam")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/kullanici/repoadi/main/script.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HilalWare-Official/HilalWare/refs/heads/main/test.lua",true))()
     ]])
 end
 
